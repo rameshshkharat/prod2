00394e681c47c8de5b8c14048ac298d7f50ee130
rameshshkharat <128587216+rameshshkharat@users.noreply.github.com>
2023-03-31 21:48:21 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
+sudo yum install httpd -y
 sudo chmod a+rwx /var/www/html
 sudo rm -fr /var/www/html/index.html
 sudo cp index.html /var/www/html/index.html
